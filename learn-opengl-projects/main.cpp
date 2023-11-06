715e6d1eef23a6f177410f2503c6de71ed0f1c7f
LajosPolya <lajospolya94@gmail.com>
2023-11-05 20:06:07 -0500

    use GLSL 430

--- learn-opengl-projects/main.cpp
+++ learn-opengl-projects/main.cpp
@@ -5,13 +5,13 @@
 #include <GLFW/glfw3.h>
 #include <iostream>
 
-const char* vertexShaderSource = "#version 330 core\n"
+const char* vertexShaderSource = "#version 430 core\n"
 "layout (location = 0) in vec3 aPos;\n"
 "void main()\n"
 "{\n"
 "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
 "}\0";
-const char* fragmentShaderSource = "#version 330 core\n"
+const char* fragmentShaderSource = "#version 430 core\n"
 "out vec4 FragColor;\n"
 "void main()\n"
 "{\n"
