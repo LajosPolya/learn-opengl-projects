a551624341d6ca8ac1ffe4799250a143278ce337
LajosPolya <lajospolya94@gmail.com>
2024-01-14 18:24:02 -0500

    remove unneeded variables

--- lighting-ambient/main.cpp
+++ lighting-ambient/main.cpp
@@ -29,8 +29,6 @@ const unsigned int SCR_HEIGHT = 600;
 Camera camera = Camera(glm::vec3(0.0f, 0.0f, 5.0f));
 
 bool firstMouse = true;
-float yaw = -90.0f;
-float pitch = 0.0f;
 float lastX = 800.0f / 2.0;
 float lastY = 600.0 / 2.0;
 float fov = 45.0f;
